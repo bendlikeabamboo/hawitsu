bbcf125c631f3e08d866d0ce9de6d39408fe385e
bendlikeabamboo <mbalmeo32@gmail.com>
2023-09-09 22:04:16 +0800

    Learning Javascript

---
+++ poking_at_full_stack/learn_javascript/onClick.js
@@ -0,0 +1,15 @@
+function buttonAlert() {
+    alert("You have clicked the function");
+    alert("This is another effect of clicking the button");
+    alert(Date());
+}
+
+function changeText() {
+    document.getElementById("header-id-001").innerHTML="It did";
+
+}
+
+function changeBackground() {
+    document.getElementById("header-id-002").innerHTML="Yes you did!";
+    document.getElementById("button-id-001").style.backgroundColor="green";
+}
